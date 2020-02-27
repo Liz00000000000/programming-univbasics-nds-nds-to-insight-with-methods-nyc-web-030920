9ce4f7955f16f9b2207fb6c7f582ee8995092915
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-02-27 01:44:49 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -14,12 +14,12 @@ def gross_for_director(nds)
   #return array
   total = [ ]
    index = 0
-     while index < directors_database[nds][:movies].size do
-       x = directors_database[nds][:movies][index][:worldwide_gross]
+     while index < directors_database[0][:movies].size do
+       x = directors_database[0][:movies][index][:worldwide_gross]
      total << x
      index += 1
    end
-   return total.sum
+   return total
 end
 
 # Write a method that, given an NDS creates a new Hash
